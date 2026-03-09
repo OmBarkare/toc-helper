ed154600faee23bad1f714fb360e976982758cc8
omomo <om062006@gmail.com>
2026-03-09 23:11:26 +0530

    feat: add parsing logic
    parsing logic for everything except transitions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs, io::Read};
+use std::{default, fs, io::Read};
 
+#[derive(Debug)]
 struct FiniteAutomata {
     alphabets: Vec<String>,
     states: Vec<String>,
@@ -7,10 +8,70 @@ struct FiniteAutomata {
     initial: String,
     accept: Vec<String>,
 }
+
+impl FiniteAutomata {
+    fn init() -> FiniteAutomata {
+        FiniteAutomata {
+            alphabets: Vec::new(),
+            states: Vec::new(),
+            transition: Vec::new(),
+            initial: String::new(),
+            accept: Vec::new(),
+        }
+    }
+}
 fn main() {
     let config = fs::read_to_string("config.txt").expect("failed to read config file");
-    let lines = config.split("\n");
-    for line in lines {
-        print!("{line}\n");
+    let mut lines = config.split("\n");
+
+    let mut line;
+    let mut section;
+    let mut finiteAutomata = FiniteAutomata::init();
+    loop {
+        line = lines.next().unwrap().split(" ");
+        section = line.next().unwrap();
+        // if section == "end" {
+        //     break;
+        // }
+        match section {
+            "alphabets:" => {
+                println!("{}", section);
+                for element in line {
+                    println!("{:?}", element);
+                    finiteAutomata.alphabets.push(element.to_string());
+                }
+            }
+            "states:" => {
+                println!("{}", section);
+                for element in line {
+                    println!("{:?}", element);
+                    finiteAutomata.states.push(element.to_string());
+                }
+            }
+            "initial:" => {
+                println!("{}", section);
+                for element in line {
+                    println!("{:?}", element);
+                    finiteAutomata.initial = element.to_string();
+                }
+            }
+            "transition:" => {
+                println!("in transition");
+            }
+            "final:" => {
+                println!("{}", section);
+                for element in line {
+                    println!("{:?}", element);
+                    finiteAutomata.accept.push(element.to_string());
+                }
+            }
+            "end" => {
+                break;
+            }
+            default => {
+                print!("this is the section ->{}", section);
+            }
+        }
     }
+    println!("{:?}", finiteAutomata);
 }
