d88f9f5425f9109378338ad261a88ce1eb7723d1
omomo <om062006@gmail.com>
2026-03-14 23:16:26 +0530

    feat: add correct minimisation logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{collections::{HashMap}, fs};
+use std::{
+    collections::{BTreeMap, BTreeSet, HashMap, btree_map::Keys},
+    fs,
+    hash::Hash,
+    io::BufRead,
+};
 
 #[derive(Debug)]
 struct FiniteAutomata {
@@ -22,19 +27,110 @@ impl FiniteAutomata {
 
     // should return another FiniteAutomata struct
     fn minimise(&self) {
-
         // ----------------- SETUP -----------------
-        
+
+        // each index represents a equivalance set
+        let mut e_classes: Vec<BTreeSet<String>> = Vec::new();
+        let mut temp_eclasses: Vec<BTreeSet<String>> = Vec::new();
+
+        e_classes.push(BTreeSet::new());
+        e_classes.push(BTreeSet::new());
+        let mut track_all: HashMap<String, u32> = HashMap::new();
+
         // removing unreachable states
 
-        // splitting final and non-final states
+        // splitting final and non-final states and initializing track
+        for element in &self.states {
+            if self.accept.contains(element) {
+                e_classes[1].insert(element.clone());
+                track_all.insert(element.clone(), 1);
+            } else {
+                e_classes[0].insert(element.clone());
+                track_all.insert(element.clone(), 0);
+            }
+        }
 
         // ---------------- MAIN LOOP ----------------
 
-        loop {}
+        loop {
+            let mut buf_vector: Vec<BTreeSet<String>> = Vec::new();
 
+            for class in &e_classes {
+                if class.is_empty() {
+                    continue;
+                }
+
+                let mut track_class: HashMap<String, isize> = HashMap::new();
+                for s in class {
+                    track_class.insert(s.clone(), -1);
+                }
+                
+                //first element loop
+                for (i, element) in class.iter().enumerate() {
+                    if track_class[element] == -1 {
+                        if let Some(ecidx) = track_class.get_mut(element) {
+                            *ecidx = i as isize;
+                        }
+                    } else {
+                        continue;
+                    }
+                    let mut v1: Vec<u32> = Vec::new(); // tuple of element1, initialized everytime
+                    for alphabet in &self.alphabets {
+                        v1.push(track_all[&self.transition[element][alphabet]]);
+                    }
+
+                    // second element loop
+                    for (k, element2) in class.iter().enumerate() {
+                        if track_class[element2] == -1 {
+                            let mut v2: Vec<u32> = Vec::new(); // tuple of element2, initialized everytime
+                            for alphabet in &self.alphabets {
+                                v2.push(track_all[&self.transition[element2][alphabet]]);
+                            }
+
+                            if v1 == v2 {
+                                if let Some(ecidx) = track_class.get_mut(element2) {
+                                    *ecidx = i as isize;
+                                }
+                            }
+                        }
+                    }
+                }
+                // println!("eclass_idx -> {:?}", eclass_idx); //debug
+
+                let mut new_classes: HashMap<isize, BTreeSet<String>> = HashMap::new();
+                for (element, class_no) in track_class {
+                    new_classes
+                        .entry(class_no)
+                        .or_insert_with(BTreeSet::new)
+                        .insert(element);
+                }
+                for (_, temp_btree) in new_classes {
+                    buf_vector.push(temp_btree);
+                }
+            }
+
+            println!("bif_vector: {:?}", buf_vector);
+
+            buf_vector.sort();
+            temp_eclasses = e_classes.clone();
+            e_classes = buf_vector;
+
+            println!("t->{:?}\ne->{:?}", temp_eclasses, e_classes);
+            if temp_eclasses == e_classes {
+                break;
+            }
+
+            for (i, class) in e_classes.iter().enumerate() {
+                for element in class {
+                    if let Some(t) = track_all.get_mut(element) {
+                        *t = i as u32;
+                    }
+                }
+            }
+        }
     }
 }
+
 fn main() {
     let config = fs::read_to_string("config.txt").expect("failed to read config file");
 
@@ -103,10 +199,18 @@ fn main() {
                     line = str.split(" ");
                     let state = line.next().unwrap().to_string();
                     println!("{}", state);
-                    finiteAutomata.transition.insert(state.clone(), HashMap::new());
+                    finiteAutomata
+                        .transition
+                        .insert(state.clone(), HashMap::new());
                     if let Some(hmap) = finiteAutomata.transition.get_mut(&state) {
-                        hmap.insert(line.next().unwrap().to_string(), line.next().unwrap().to_string());
-                        hmap.insert(line.next().unwrap().to_string(), line.next().unwrap().to_string());
+                        hmap.insert(
+                            line.next().unwrap().to_string(),
+                            line.next().unwrap().to_string(),
+                        );
+                        hmap.insert(
+                            line.next().unwrap().to_string(),
+                            line.next().unwrap().to_string(),
+                        );
                     }
                 }
             }
@@ -126,4 +230,5 @@ fn main() {
         }
     }
     println!("{:#?}", finiteAutomata);
+    finiteAutomata.minimise();
 }
